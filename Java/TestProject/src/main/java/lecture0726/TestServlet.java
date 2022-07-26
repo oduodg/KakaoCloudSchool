be99ef88f50ff05febc43269618b1127c2df1a10
oduodg <werewildfire@gmail.com>
2022-07-26 16:23:52 +0900

    2022.07.26 JSP practice project

---
+++ Java/TestProject/src/main/java/lecture0726/TestServlet.java
@@ -0,0 +1,50 @@
+package lecture0726;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TestServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 1. 입력
+		// 2. 로직
+		// 3. 출력
+		// 결과를 client에게 돌려주기 위해서는
+		// 1. contentType을 설정
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = new PrintWriter(response.getOutputStream()); // stream 객체 생성
+		out.println("소리없는 아우성!");
+		out.close(); // 다 사용한 resource 닫아주기
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
